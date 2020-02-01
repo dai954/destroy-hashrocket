32273d675559aba07dc30af093fac983d8788eae
dai954 <da0606ha@gmail.com>
2020-02-01 19:06:08 +0900

    Update application

--- README.md
+++ README.md
@@ -1,24 +1,18 @@
-# README
+# DestroyHashRocket
+ 
+DestroyHashRocket is an app to change hash_rocket style to symbol style. 
+ 
+# DEMO
+ 
+![demo](https://raw.github.com/wiki/dai954/destroy-hashrocket/images/destroy_hashrocket_demo.gif)
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+ 
+# Usage
 
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+access to URL <https://destroy-hashrocket.herokuapp.com>
+paste text in input_box of the left side
+ 
+# Note
+ 
+ I tested only ruby and haml text.
+ After copy origin somewhere, use this app.
--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,23 +1,17 @@
 $(function(){
-  console.log("ok")
   
-  $("#the_html_body").on("keyup", function() {
+  $("#the_html_body").on("input", function() {
     
     input = $("#the_html_body").val()
 
       // 先シンボル型
-      let output1 = input.replace(/:(\w+)\s{0,1}=>/g, '$1:')
+      let output_symbolType = input.replace(/:([^ ]+)\s{0,}=>/g, '$1:')
       // 文字列型
-      let output2 = output1.replace(/\"(\w+)\"\s{0,1}=>/g, '$1:')
-      // 半角英数字と記号(先シンボル型) 最後に半角スペースがこないように
-      let output3 = output2.replace(/:([ -~]+[!-~])\s{0,1}=>/g, '$1:')
-      // 半角英数字と記号(文字列型) 最後に半角スペースがこないように
-      let output4 = output3.replace(/\"([ -~]+[!-~])\"\s{0,1}=>/g, '$1:')
-
-
+      let output_stringType1 = output_symbolType.replace(/\"([^\"]+[^ ])\s{0,}\"\s{0,}=>/g, '\"$1\":')
+      let output_stringType2 = output_stringType1.replace(/\'([^\']+[^ ])\s{0,}\'\s{0,}=>/g, '\'$1\':')
       let list = $("#symbol-output");
-      list.append(output4)
-  })
+      list.append(output_stringType2);
+  });
+});
 
 
-  })
